1c4f5834500d256073374bb8fa09f53041d8a3db
Adam Reed <thirdeyeadam@gmail.com>
2021-04-28 18:48:14 -0500

    updated model index with foreign keys

--- models/index.js
+++ models/index.js
@@ -6,29 +6,27 @@ const ProductTag = require('./ProductTag');
 
 // Products belongsTo Category
 Products.belongsTo(Category, {
-
-
+  foreignKey: 'category_id',
 });
 
 // Categories have many Products
 Category.hasMany(Products, {
-
-
+  foreignKey: 'category_id',
 });
 
 
 // Products belongToMany Tags (through ProductTag)
-Products.belongsToMany(Tags, {
-
-
+Product.belongsToMany(Tags, {
+  through: ProductTag,
+  foreignKey: 'product_id',
 });
 
 
 // Tags belongToMany Products (through ProductTag)
 
 Tags.belongsToMany(Products, {
-
-
+    through: ProductTag,
+    foreignKey: 'tag_id',
 });
 
 
