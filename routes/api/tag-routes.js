a40d19f4415f69e1748895e7ff1b29508b94d1a1
Adam Reed <thirdeyeadam@gmail.com>
2021-04-29 08:04:08 -0500

    corrected res.status so tags display based on id search in the URL

--- routes/api/tag-routes.js
+++ routes/api/tag-routes.js
@@ -29,7 +29,7 @@ router.get('/:id', async (req, res) => {
       return;
     }
 
-    res.status(200).json(productCardData);
+    res.status(200).json(tagCardData);
   } catch (err) {
     res.status(500).json(err);
   }
